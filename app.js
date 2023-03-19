82285b64864d2947977834234f729e5ae2ea45c1
Xtreme-Flash <doorway125@gmail.com>
2023-03-19 19:20:20 +0530

    v1

---
+++ app.js
@@ -0,0 +1,15 @@
+//fuction to clear the screen
+function clearScreen(){
+    document.getElementById("result").value = "";
+}
+
+// function to display values
+function display(value){
+    document.getElementById("result").value += value;
+}
+
+function calculate(){
+    var p = document.getElementById("result").value;
+    var q = eval(p);
+    document.getElementById("result").value = q;
+}
